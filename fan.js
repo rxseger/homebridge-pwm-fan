37f71b334ebaa0b50eebd6d4da26b0dd3a54cb10
rxseger <rseger@gmx.co.uk>
2016-10-09 07:19:34 +0000

    Use pigpiod, read interrupts, pwmWrite, and process.hrtime

--- fan.js
+++ fan.js
@@ -1,5 +1,7 @@
 'use strict';
 
+const Gpio = require('pigpio').Gpio;
+
 let Service, Characteristic;
 
 module.exports = (homebridge) => {
@@ -15,6 +17,24 @@ class FanPlugin
     this.log = log;
     this.name = config.name;
 
+    this.tachometer = new Gpio(16, { // physical #36, BCM 16
+      mode: Gpio.INPUT,
+      pullUpDown: Gpio.PUD_UP,
+      edge: Gpio.FALLING_EDGE
+    });
+    this.last_tach = process.hrtime();
+    this.tachometer.on('interrupt', this.onTachometer.bind(this));
+
+    this.motor = new Gpio(23, { // physical #16, BCM 23
+      mode: Gpio.OUTPUT
+    });
+    const dutycycle = 0.996;
+    this.motor.pwmWrite(Math.round(dutycycle * 255));
+
+
+setInterval( () => {}, 1000);
+return;
+
     this.fan = new Service.Fan(this.name);
     this.fan
       .getCharacteristic(Characteristic.On)
@@ -23,6 +43,17 @@ class FanPlugin
       .getCharacteristic(Characteristic.RotationSpeed)
       .on('get', this.getRotationSpeed.bind(this))
       .on('set', this.setRotationSpeed.bind(this));
+  }
+
+  onTachometer() {
+    const difference = process.hrtime(this.last_tach);
+    const dt = difference[0] + difference[1] / 1e9; // s + ns --> in seconds
+    if (dt < 0.02) return; // reject spuriously short pulses
+ 
+    const freq = 1 / dt;
+    const rpm = (freq / 2) * 60;
+    console.log(rpm);
+    this.last_tach = process.hrtime();
   }
 
   getOn(cb) {
@@ -42,3 +73,4 @@ class FanPlugin
   }
 }
 
+new FanPlugin({}, {});
